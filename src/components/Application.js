95527afbb0973ed61b82dca78c042b35dd554641
Briana Endanawas <brianamay48@gmail.com>
2022-03-06 07:12:49 -0500

    create getInterview helper function

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,16 +3,30 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 import axios from "axios";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application() {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {} 
+    appointments: {} ,
+    interviewers: {}
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+  
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   const setDay = day => setState({ ...state, day });
 
@@ -23,6 +37,7 @@ export default function Application() {
       axios.get('/api/interviewers')
     ]).then((all) => {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      console.log(all[2].data);
     });
   }, []);
 
@@ -49,12 +64,7 @@ export default function Application() {
       />
       </section>
       <section className="schedule">
-        {dailyAppointments.map((appointment) => (
-          <Appointment 
-            key={appointment.id}
-            {...appointment}
-          />
-        ))}
+        {schedule}
       </section>
       
     </main>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -7,3 +7,18 @@ export function getAppointmentsForDay(state, day) {
   });
   return appointmentList;
 }
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  } else {
+    return {
+      "student": interview.student,
+      "interviewer": {  
+        "id": state.interviewers[interview.interviewer].id,
+        "name": state.interviewers[interview.interviewer].name,
+        "avatar": state.interviewers[interview.interviewer].avatar 
+      }
+    }
+  }
+}
