8f91e6569b5af305f27ade2ebbfd8002e837e168
Briana Endanawas <brianamay48@gmail.com>
2022-03-15 00:41:07 -0400

    update spots remaining

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -18,9 +18,19 @@ export function useApplicationData() {
       axios.get('/api/interviewers')
     ]).then((all) => {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-      console.log(all[2].data);
     });
   }, []);
+  
+  function updateSpots() {
+    const days = [...state.days];
+    for (let day of days) {
+      const interviews = day.appointments.map(id => state.appointments[id].interview);
+      const nulls = interviews.filter(interview => interview === null);
+      day.spots = nulls.length;
+    } 
+    return days; 
+  }
+  updateSpots();
 
   function bookInterview(id, interview) {
     const appointment = {
@@ -38,7 +48,7 @@ export function useApplicationData() {
       setState({
         ...state,
         appointments
-      }); 
+      });
     })
   }
 
@@ -58,7 +68,7 @@ export function useApplicationData() {
       setState({
         ...state,
         appointments
-      }); 
+      });
     })
   }
 
